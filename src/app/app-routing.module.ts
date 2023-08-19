c5077b982a903e9ff5907fce8220f83f3217f042
Joseph Juri <joseph.juri@student.tugraz.at>
2023-08-19 17:13:25 +0200

    Updated Routing for "Example" API call

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainFrameComponent } from './components/main-frame/main-frame.component';
 import { PipelineCarouselComponent } from './components/pipeline-carousel/pipeline-carousel.component';
+import { ExampleComponent } from './components/example/example.component';
 
 const routes: Routes = [
   { path: '', component: PipelineCarouselComponent},
+  { path: 'example', component: ExampleComponent},
   { path: '**', redirectTo: '' }
 ];
 
